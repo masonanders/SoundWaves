d75fcdf86adcda3ec56f2450a65d6bc2f25b072c
MasonA <mason@ourprairie.net>
2018-08-07 14:27:49 -0700

    Finish users controller

--- SoundWaves/app/controllers/api/users_controller.rb
+++ SoundWaves/app/controllers/api/users_controller.rb
@@ -1,13 +1,36 @@
 class Api::UsersController < ApplicationController
-  def create
-  end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages
+    end
   end
 
   def destroy
+    logout!
+    redirect_to new_session_url # TODO make sure this is the correct session_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- SoundWaves/config/routes.rb
+++ SoundWaves/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/create'
-    get 'users/show'
-    get 'users/update'
-    get 'users/destroy'
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:show, :create, :update, :destroy]
   end
   root "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
