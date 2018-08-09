f5013d3b9c3ee3240a467af2558f1fbaaaed9c9f
MasonA <mason@ourprairie.net>
2018-08-08 23:25:11 -0700

    Start login modals

--- SoundWaves/frontend/components/splash/splash_banner.jsx
+++ SoundWaves/frontend/components/splash/splash_banner.jsx
@@ -6,8 +6,10 @@ export default () => (
     <div className='splash-logo'>
     </div>
 
-    <button id='sign-in' className='session-button'>Sign in</button>
-    <button id='create-account' className='session-button'>Create account</button>
+    <div className='session-buttons'>
+      <button id='sign-in'>Sign in</button>
+      <button id='create-account'>Create account</button>
+    </div>
 
     <div className='splash-header'>
       <h1>Discover new music with SoundWaves</h1>
@@ -17,8 +19,8 @@ export default () => (
       <h3>Upload and share music with artists who have a living room, a microphone, and a dream.</h3>
     </div>
 
-    <div className='upload-today'>
-      <button id='start-today'>Start uploading now</button>
+    <div className='demo-login'>
+      <button className='big-button'>Try a demo log in</button>
     </div>
   </section>
 );
--- SoundWaves/frontend/components/splash/splash_search.jsx
+++ SoundWaves/frontend/components/splash/splash_search.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 export default () => {
   return (
     <section className="splash-search" >
-      <h2>THIS IS A TEST</h2>
-      <input type='search' placeholder='Search for artists or tracks!' />
+      <input className="splash-searchbar" type='search' placeholder='Search for artists or tracks!' />
+      <button id="search-button"></button>
+      <div className="splash-searchbar-or">
+        <h4>or</h4>
+      </div>
+      <button className='big-button'>Upload your own</button>
     </section>
   );
 };
